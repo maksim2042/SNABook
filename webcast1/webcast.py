4544141ba9065e9e818b739d7db39d2a32c71c3e
Max <maxt@deepmile.com>
2011-11-09 01:10:56 -0500

    Added slides, fixed code, cooked dinner, swept the floor...

--- webcast1/webcast.py
+++ webcast1/webcast.py
@@ -1,13 +1,64 @@
 import json
 import heatmap
+import networkx as net
+import matplotlib.pyplot as plot
+
+def trim_degrees(g, degree=1):
+    """
+    Trim the graph by removing nodes with degree less then value of the degree parameter
+    Returns a copy of the graph, so it's non-destructive.
+    """
+    g2=g.copy()
+    d=net.degree(g2)
+    for n in g2.nodes():
+        if d[n]<=degree: g2.remove_node(n)
+    return g2
+
+def sorted_degree(g):
+    d=net.degree(g)
+    ds = sorted(d.iteritems(), key=lambda (k,v): (-v,k))
+    return ds
+
+def add_or_inc_edge(g,f,t):
+    """
+    Adds an edge to the graph IF the edge does not exist already. 
+    If it does exist, increment the edge weight.
+    Used for quick-and-dirty calculation of projected graphs from 2-mode networks.
+    """
+    if g.has_edge(f,t):
+        g[f][t]['weight']+=1
+    else:
+        g.add_edge(f,t,weight=1)
 
 file="data.json"
-
 i = open(file,'rb')
 
-points =[]
+retweets=net.DiGraph()
+hashtag_net=net.Graph()
+
 for tweet in i:
     js=json.loads(tweet)
+    
+    ### process tweet to extract information
+    try:
+        author=js['user']['screen_name']
+        entities=js['entities']
+        mentions=entities['user_mentions']
+        hashtags=entities['hashtags']
+    
+        for rt in mentions:
+            alter=rt['screen_name']
+            retweets.add_edge(author,alter)
+        
+        tags=[str.lower(tag['text']) for tag in hashtags]
+        for t1 in tags:
+            for t2 in tags:
+                if t1 is not t2:
+                    add_or_inc_edge(hashtag_net,t1,t2)      
+    except KeyError:
+        print ':-('
+        continue
+    
     
     place=''
     try:
@@ -40,3 +91,4 @@ hm = heatmap.Heatmap()
 hm.heatmap(points, "hm.png", dotsize=10)
 hm.saveKML("geo.kml")
 
+points =[]
